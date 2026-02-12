38a33639e5d927580af08060add2b5c84132ef52
Satyajeet <satyajeetsonne@gmail.com>
2026-02-13 01:45:16 +0530

    CORS: restrict allow_methods to explicit list

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ app.add_middleware(
         "http://localhost:5173",
     ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
